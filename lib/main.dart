db56112e92ebc0028cf3d5cba7b8faa4da55b148
Nevil Vaghasiya <43700430+Nevilrv@users.noreply.github.com>
2022-03-11 23:15:10 +0530

    init[m12]

--- lib/main.dart
+++ lib/main.dart
@@ -30,7 +30,7 @@ void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
   await GetStorage.init();
-  //milan patel
+  //milan patel amreliya
   runApp(MyApp());
 }
 // home: ValidationScreen(),
